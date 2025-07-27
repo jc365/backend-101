37e7853007856ac572860e3a3c1380f083f2467d
Juan <jcsantamar@gmail.com>
2025-07-27 21:56:35 +0200

    Limpieza previa al despliegue en Render

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get("/", (req, res) => {
 
 // Work's routes imported
 app.use("/scrap", scrapRoutes);
-app.use("/users", userRoutes);
+app.use("/api/users", userRoutes);
 app.use("/api/notes", noteRoutes);
 
 app.listen(PORT, () => {
