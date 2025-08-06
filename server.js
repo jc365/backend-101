2dfc6d19f56ea0833d9b8e442f7926a2a6256582
Juan <jcsantamar@gmail.com>
2025-08-07 00:25:47 +0200

    Limpieza de elementos innecesarios

--- server.js
+++ server.js
@@ -1,21 +1,29 @@
 import express from "express";
 import mongoose from "mongoose";
-import dotenv from "dotenv";
+import dotenv, { config } from "dotenv";
 import cors from "cors";
 import registerAutomaticRoutes from "./APIS/_nucleo/registerAutomaticRoutes.js";
 import { ROOT_DIR, pathJoin } from "./APIS/_nucleo/toolPaths.mjs";
 //-- Static routes
-import userRoutes from "./routes/userRoutes.js";
-// import noteRoutes from "./routes/noteRoutes.js";
-// import scrapRoutes from "./routes/scrapRoutes.js";
+import scrapRoutes from "./routes/scrapRoutes.js";
 import reboundRoutes from "./routes/reboundRoutes.js";
-// import apiScrapRoutes from "./APIS/scrap/Routes.js";
-// import apiNoteRoutes from "./APIS/note/Routes.js";
 import staticPagesRoutes from "./routes/staticPagesRoutes.js";
 
 dotenv.config();
 const PORT = process.env.PORT || 5050;
+const NODE_ENV = process.env.NODE_ENV || "production";
 const MONGO_URI = process.env.MONGODB_URI;
+const BACKEND_URL =
+  NODE_ENV == "local"
+    ? `http://localhost:${PORT}`
+    : "https://backend-101-hu0a.onrender.com";
+
+const CONTEXT = {
+  PORT,
+  NODE_ENV,
+  BACKEND_URL,
+};
+console.log("Execution context:", CONTEXT);
 
 const app = express();
 app.use(cors());
@@ -27,29 +35,19 @@ mongoose
   .then(() => console.log("🔵 Succesfully conect to MongoDB Atlas"))
   .catch((error) => console.error("Error conecting to MongoDB:", error));
 
-app.get("/", (req, res) => {
-  res.send("Server ready (id:sntmr101chrzdhi83xyk4u3se4jckldfjl)");
-});
-
-//-- Mount static routes (imported)
-// app.use("/scrap", scrapRoutes);
+//========  R U T A S  ==========
+//-- Root route - Ready message with identification
+app.get("/", (req, res) => res.send("Server ready (id:sntmr101chrzdhi83jld)"));
+//-- Mount static routes (depending on import's)
+app.use("/scrap", scrapRoutes); //--usada desde el front de app-jobs
 app.use("/rebound", reboundRoutes);
-app.use("/api/users", userRoutes);
-// app.use("/api/notes", apiNoteRoutes);
-// app.use("/api/notes", noteRoutes);
-// app.use("/api/scraps", apiScrapRoutes);
 app.use("/static", staticPagesRoutes);
-
-//-- Mount dynamic routes
+//-- Mount dynamic routes (depending on directories structure)
 await registerAutomaticRoutes(app, pathJoin(ROOT_DIR, "APIS"));
 
 // Middleware for routes not found
-app.use((req, res) => {
-  res.status(404).send(`Cannot GET ${req.originalUrl}`);
-});
+app.use((req, res) => res.status(404).send(`Cannot GET ${req.originalUrl}`));
 
-app.listen(PORT, () => {
-  console.log(`🔵 Server listening at http://localhost:${PORT}`);
-});
+app.listen(PORT, () => console.log(`🔵 Server listening at ${BACKEND_URL}`));
 
 export default app;
